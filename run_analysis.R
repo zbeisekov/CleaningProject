d20464ee6700a579eb6976d05a72297f4bfdb564
Zhenis Beisekov <zhenis.beisekov@gmail.com>
2019-11-07 23:50:27 +0100

    Cleaned up the cleaning

--- run_analysis.R
+++ run_analysis.R
@@ -33,13 +33,13 @@ subject <- rbind(subject_train, subject_test)
 merged <- cbind(subject, y, x)
 
 # Step 2: Extracts only the measurements on the mean and standard deviation for each measurement.
-tidy_data <- merged %>% select(subject, code, contains("mean"), contains("std"))
+tidy_data <- merged %>% select(subject, code, contains("mean."), contains("std."))
 
 # Step 3: Uses descriptive activity names to name the activities in the data set.
-tidy_data$code <- activities[tidy_data$code, 2]
+tidy_data$code <- activities$activity[tidy_data$code]
 
 # Step 4: Appropriately labels the data set with descriptive variable names.
-names(tidy_data)[2] = "activity"
+tidy_data <- rename(tidy_data, activity = code)
 names(tidy_data)<-gsub("Acc", "Accelerometer", names(tidy_data))
 names(tidy_data)<-gsub("Gyro", "Gyroscope", names(tidy_data))
 names(tidy_data)<-gsub("BodyBody", "Body", names(tidy_data))
@@ -47,11 +47,11 @@ names(tidy_data)<-gsub("Mag", "Magnitude", names(tidy_data))
 names(tidy_data)<-gsub("^t", "Time", names(tidy_data))
 names(tidy_data)<-gsub("^f", "Frequency", names(tidy_data))
 names(tidy_data)<-gsub("tBody", "TimeBody", names(tidy_data))
-names(tidy_data)<-gsub("-mean()", "Mean", names(tidy_data), ignore.case = TRUE)
-names(tidy_data)<-gsub("-std()", "STD", names(tidy_data), ignore.case = TRUE)
-names(tidy_data)<-gsub("-freq()", "Frequency", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("\\.mean\\.+", "Mean", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("\\.std\\.+", "STD", names(tidy_data), ignore.case = TRUE)
 names(tidy_data)<-gsub("angle", "Angle", names(tidy_data))
 names(tidy_data)<-gsub("gravity", "Gravity", names(tidy_data))
+names(tidy_data)<-gsub("\\.+", "", names(tidy_data))
 
 # Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 final <- tidy_data %>%
